271376a6142b2debc7c7e22787ad38a5b6f6a98f
MaxineFonua <maxinefonua@gmail.com>
2025-09-30 15:43:11 -0700

    functional tests main running

--- voyager-tests/src/main/java/org/voyager/tests/airports/AirportsTest.java
+++ voyager-tests/src/main/java/org/voyager/tests/airports/AirportsTest.java
@@ -1,54 +1,49 @@
-package org.voyager.test;
+package org.voyager.tests.airports;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.voyager.config.VoyagerConfig;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.voyager.tests.config.FunctionalTestConfig;
 import org.voyager.model.airport.AirportPatch;
 import org.voyager.model.airport.AirportType;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-import static org.voyager.utils.ConstantsUtils.*;
+import org.voyager.utils.ConstantsUtils;
 
 public class AirportsTest {
     private static RequestSpecification requestSpec;
-    @BeforeClass
-    public static void setup() {
-        String portConfig = VoyagerConfig.getProperty("voyager.port");
-        if (portConfig != null && !portConfig.trim().isEmpty()) {
-            RestAssured.port = Integer.parseInt(portConfig);
-        }
-        RestAssured.basePath = VoyagerConfig.getProperty("voyager.path.airports");
-        String authToken = VoyagerConfig.getProperty("voyager.auth.token");
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = FunctionalTestConfig.getBaseUrl();
+        RestAssured.basePath = FunctionalTestConfig.getAirportsConfig().getAirportsPath();
+        String authToken = FunctionalTestConfig.getAuthToken();
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         requestSpec = new RequestSpecBuilder()
                 .addHeader("Accept", "application/json")
-                .addHeader(AUTH_TOKEN_HEADER_NAME, authToken)
+                .addHeader(ConstantsUtils.AUTH_TOKEN_HEADER_NAME, authToken)
                 .build();
     }
 
     @Test
     public void testGetIataCodes() {
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
-                .basePath(VoyagerConfig.getProperty("voyager.path.iata"))
+                .basePath(FunctionalTestConfig.getAirportsConfig().getIataPath())
                 .when()
                 .get()
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("size()", greaterThan(0)) // Assert the response is a non-empty array
-                .body("$", hasItem("HNL"));
+                .body("size()", Matchers.greaterThan(0)) // Assert the response is a non-empty array
+                .body("$", Matchers.hasItem("HNL"));
     }
 
     @Test
     public void testPostIata() {
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
-                .basePath(VoyagerConfig.getProperty("voyager.path.iata"))
+                .basePath(FunctionalTestConfig.getAirportsConfig().getIataPath())
                 .when()
                 .post()
                 .then()
@@ -58,34 +53,34 @@ public class AirportsTest {
 
     @Test
     public void testGetAirports() {
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
                 .when()
                 .get()
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("size()", greaterThan(0)) // Assert the response is a non-empty array
-                .body("iata", hasItem("HNL"));
+                .body("size()", Matchers.greaterThan(0)) // Assert the response is a non-empty array
+                .body("iata", Matchers.hasItem("HNL"));
     }
 
     @Test
     public void testGetAirport() {
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
                 .when()
                 .get("/ITM")
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("subdivision",equalTo("Hyogo"));
+                .body("subdivision", Matchers.equalTo("Hyogo"));
     }
 
     @Test
     public void testPatchAirport() {
         AirportPatch airportPatch1 = AirportPatch.builder().type(AirportType.UNVERIFIED.name()).build();
         AirportPatch airportPatch2 = AirportPatch.builder().type(AirportType.CIVIL.name()).build();
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body(airportPatch1)
@@ -94,11 +89,10 @@ public class AirportsTest {
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("type",equalTo(AirportType.UNVERIFIED.name()));
+                .body("type", Matchers.equalTo(AirportType.UNVERIFIED.name()));
 
-        given()
+        RestAssured.given()
                 .spec(requestSpec)
-                .basePath(VoyagerConfig.getProperty("voyager.path.airports"))
                 .contentType(ContentType.JSON)
                 .body(airportPatch2)
                 .when()
@@ -106,6 +100,6 @@ public class AirportsTest {
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("type",equalTo(AirportType.CIVIL.name()));
+                .body("type", Matchers.equalTo(AirportType.CIVIL.name()));
     }
 }
